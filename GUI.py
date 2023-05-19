477309a7a38750698c1f3c9a535ecc340422b181
Vitalii <vmarlyan@mail.ru>
2023-05-19 23:43:12 +0300

    Add GUI functionallty

--- GUI.py
+++ GUI.py
@@ -1,9 +1,10 @@
-from tkinter import Frame, Tk, Button, GROOVE, LEFT, Label, LabelFrame
+from tkinter import Frame, Tk, Button, GROOVE, LEFT, Label, LabelFrame, Toplevel, Entry
 from tkinter import filedialog as fd
 from PIL import Image as PILImage
 from PIL import ImageTk
-import cv2
+import cv2, os
 import core
+import db
 
 
 class Window:
@@ -42,18 +43,18 @@ class Window:
         self.check_transport = Label(transport_frame)
         self.check_transport.pack(side=LEFT)
         Button(self.root, width=15, height=1, text="Заполнить договор", font=("Arial", 12, "bold"),
-               relief=GROOVE, bd=8).place(x=80, y=250)
+               relief=GROOVE, bd=8, command=self.fill_contract).place(x=80, y=250)
         Button(self.root, width=20, height=1, text="Проверить заполнение", font=("Arial", 11, "bold"),
-               relief=GROOVE, bd=8).place(x=70, y=320)
+               relief=GROOVE, bd=8, command=self.check_filling).place(x=70, y=320)
+        Button(self.root, width=20, height=1, text="Внести договор в БД", font=("Arial", 11, "bold"),
+               relief=GROOVE, bd=8, command=self.add_contract_to_db).place(x=70, y=420)
         Button(self.root, width=20, height=1, text="Отбор договоров", font=("Arial", 11, "bold"),
-               relief=GROOVE, bd=8).place(x=120, y=600)
+               relief=GROOVE, bd=8, command=self.contract_selection).place(x=120, y=600)
         self.label = Label(output_frame)
         self.label.pack()
 
     def open_image(self, par: str):
         image_path = fd.askopenfilename()
-        image_file = fd.askopenfile()
-        # print(image_name)
         img = PILImage.open(image_path)
         if (img.width or img.height) >= 2800:
             img = img.reduce(7)
@@ -75,11 +76,11 @@ class Window:
             self.passport_rec(image_path)
         else:
             self.put_tick(par)
-        # print(ocr_recognition(image_file))
-        # print(core.ocr_recognition(image_name))
+            self.tc_rec(image_path)
         return image_path
 
-    def passport_rec(self, image_path):
+    @staticmethod
+    def passport_rec(image_path):
         img_proc = core.ImageProcessing(image_path)
         image = img_proc.image_preparation()
         res_img = img_proc.resizing(image, new_width=818, new_height=1162, interp=cv2.INTER_LINEAR)
@@ -92,6 +93,49 @@ class Window:
         result_rotate = img_proc.ocr_recognition(crop_image_rotate)
         results = [result_up, result_down, result_rotate]
         img_proc.insert_data_word(results)
+
+    @staticmethod
+    def tc_rec(image_path):
+        img_proc = core.TCProcessing(image_path)
+        image = img_proc.image_preparation()
+        res_img = img_proc.resizing(image, new_width=818, new_height=1162, interp=cv2.INTER_LINEAR)
+        crop_image_up = img_proc.crop_image(res_img, side="up")
+        crop_image_down = img_proc.crop_image(res_img, side="down")
+        result_up = img_proc.ocr_recognition(crop_image_up)
+        result_down = img_proc.ocr_recognition(crop_image_down)
+        results_two_pages = [result_up, result_down]
+        img_proc.insert_data_word(results_two_pages)
+
+    @staticmethod
+    def fill_contract():
+        core.ImageProcessing.render_word()
+
+    @staticmethod
+    def check_filling():
+        try:
+            contract_dict = core.ImageProcessing.context
+            series = contract_dict["series"]
+            number = contract_dict["number"]
+            os.startfile(f'all_doc\GNZ_result{series}{number}.docx', 'edit')
+        except KeyError:
+            print('Загружено только ТС без паспорта! ')
+            os.startfile(f'all_doc\GNZ_result.docx', 'edit')
+
+    @staticmethod
+    def add_contract_to_db():
+        contract_dict = core.ImageProcessing.context
+        if len(contract_dict) > 6:
+            name = contract_dict["name"]
+            surname = contract_dict["surname"]
+            patronymic = contract_dict["patronymic"]
+            date = "20" + contract_dict["ye"] + '-' + contract_dict["month"] + "-" + contract_dict["day"]
+            series = contract_dict["series"]
+            number = contract_dict["number"]
+            word_name = f'all_doc\GNZ_result{series}{number}.docx'
+            db.insert_data(name, surname, patronymic, series, number, date, word_name)
+
+    def contract_selection(self):
+        ChildWindow(self.root)
 
     def change_label_output(self):
         self.label.configure(image=self.output_image, height=550, width=450)
@@ -115,7 +159,53 @@ class Window:
         self.root.mainloop()
 
 
-if __name__ == '__main__':
-    window = Window()
-    window.run()
-    # print(window.open_image("passport"))
+class ChildWindow:
+    def __init__(self, parent):
+        self.root = Toplevel(parent)
+        self.root.geometry(f"900x700+400+50")
+        self.root.title('database search')
+        self.root.resizable(False, False)
+        self.root.config(bg='#DBDADA')
+        self.entry = None
+        self.draw_widgets()
+
+    def draw_widgets(self):
+        Label(self.root, text="Отбор по ФИО", font=("Arial", 14, "bold")).grid(row=1, column=3, pady=8)
+        Label(self.root, width=15, height=1, text="ФИО", font=("Arial", 12, "bold")).grid(row=2, column=0, ipady=4)
+        self.full_name = Entry(self.root, width=35, bd=4, font=("Arial", 12, "bold"))
+        self.full_name.grid(row=3, column=1, ipady=4)
+        Button(self.root, width=15, height=1, text="Отбор", font=("Arial", 12, "bold"),
+               relief=GROOVE, bd=8, command=self.get_full_name).grid(row=3, column=4)
+
+        Label(self.root, text="Отбор по дате создания", font=("Arial", 14, "bold")).grid(row=6, column=3, pady=10)
+        Label(self.root, width=15, height=2, text="Дата создания\n(yyyy-mm-dd)", font=("Arial", 12, "bold")).grid(row=8,
+                                                                                                                  column=0,
+                                                                                                                  ipady=4)
+        self.date = Entry(self.root, width=35, bd=4, font=("Arial", 12, "bold"))
+        self.date.grid(row=9, column=1, ipady=4)
+        Button(self.root, width=15, height=1, text="Отбор", font=("Arial", 12, "bold"),
+               relief=GROOVE, bd=8, command=self.get_date).grid(row=9, column=4)
+
+    def get_full_name(self):
+        full_name = self.full_name.get().upper()
+        print(full_name)
+        full_name_lst = full_name.split()
+        if len(full_name_lst) != 3:
+            print('Ошибка ввода')
+        else:
+            name = full_name_lst[1]
+            surname = full_name_lst[0]
+            patronymic = full_name_lst[2]
+            self.get_contract_from_db('', name, surname, patronymic)
+
+    def get_date(self):
+        date = self.date.get()
+        print(date)
+        self.get_contract_from_db(date)
+
+    @staticmethod
+    def get_contract_from_db(date, name='', surname='', patronymic=''):
+        if date:
+            db.read_data(date)
+        else:
+            db.read_data('', name, surname, patronymic)
